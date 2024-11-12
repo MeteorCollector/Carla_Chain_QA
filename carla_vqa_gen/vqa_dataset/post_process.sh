d3bc167e6238d726e52f7d2a18c7dd8884284da9
MeteorCollector <shaoyuqian2010@163.com>
2024-11-12 23:33:07 +0800

    mid ofwork 1112

--- carla_vqa_gen/vqa_dataset/post_process.sh
+++ carla_vqa_gen/vqa_dataset/post_process.sh
@@ -1,1 +1,1 @@
-python ./carla_vqa_generator_main.py --data-directory=../../data --output-graph-directory=./outgraph --path-keyframes=./keyframes --output-graph-examples-directory=./outexample --save-examples --visualize-projection
+python ./carla_vqa_generator_main.py --data-directory=../../../Bench2Drive-mini --output-graph-directory=./outgraph --path-keyframes=./keyframes --output-graph-examples-directory=./outexample --save-examples --visualize-projection
